603099b6d99d794075146ab9b09b3400c2cff7e5
ndurushima <ndu1994@gmail.com>
2025-06-10 10:19:10 -0500

    Course 2 Module 1 Create Login Tracker

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  const loginAttempt = (passwordAttempt) => {
+    if (attemptCount >= maxAttempts) {
+      return "Account locked due to too many failed login attempts."
+    }
+  
+    attemptCount ++;
 
+    if(passwordAttempt === userInfo.password) {
+      console.log("Sign in successful!");
+      } else {
+        return "Login failed. Please try again.";
+      }
+    };
+
+    return loginAttempt;
+};
+
+const userInfo = {
+  username: "user1",
+  password: "password123"
+};
+
+const login = createLoginTracker(userInfo);
+
+console.log(login("wrong"));
+console.log(login("wrongagain"));
+console.log(login("password123"));
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
